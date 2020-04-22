2b825c760326c6aa45a3dbc3257953d50e65df03
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-04-22 18:26:59 +0300

    I REALLY hope that I did it right...

---
+++ game.js
@@ -0,0 +1,25 @@
+class Game {
+    start() {
+        console.log('Game started');
+    }
+}
+
+class GameSavingData {
+
+}
+
+function readGameSaving() {
+
+}
+
+function writeGameSaving() {
+
+}
+
+import { Character } from './domain';
+export default {
+    Game,
+    GameSavingData,
+    readGameSaving,
+    writeGameSaving
+}
